e523fe639419db923d25f4048250fa6788331741
anakan01 <45832973+anakan01@users.noreply.github.com>
2020-08-05 15:43:32 +0200

    Add files via upload

--- config.h
+++ config.h
@@ -1,23 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Liberation Mono:size=15" };
+static const char *fonts[]          = { "Liberation Mono:size=12","fontawesome:size=12","JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Liberation Mono:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#5d2470";
+static const char col_cyan[]        = "#4351A4";
+static const char col_border[]      = "#46BCD1"; 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_border },
 };
 
 /* tagging */
